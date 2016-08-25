655c4efbdd11904769ec8c0365e710b11241fe64
629OKAI <intern@Robin-no-MacBook-Pro.local>
2016-08-25 13:09:36 +0900

    初？コミット！！

---
+++ Assets/script/ButtonClick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void changeScene () {
+		SceneManager.LoadScene("THETA");
+	}
+
+	public void changeScene2 () {
+		SceneManager.LoadScene ("Title");
+	}
+
+	public void changeScene3 () {
+		SceneManager.LoadScene ("HowToUse");
+	}
+}
